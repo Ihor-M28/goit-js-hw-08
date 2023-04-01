22fe38f2233cc7486a95f8abd9d83cccb61c6c2c
Ihor-M28 <114598607+Ihor-M28@users.noreply.github.com>
2023-04-01 09:24:40 +0300

    new
    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,11 @@
-// import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-import { simplelightbox } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-console.log(simplelightbox);
+console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
 
@@ -20,31 +19,18 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>`;
     })
     .join("");
 }
       
 function onClickGalleryItem(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
-    return
-  }
-    
-const instance = basicLightbox.create(`
-    <img src= ${evt.target.dataset.source} width="800" height="600">
-`)
-
-instance.show()
-  }
+    evt.preventDefault();
+}
+var lightbox = new SimpleLightbox(".gallery__link", { captionsData: 'alt', captionDelay: 250, });
 
 
 
